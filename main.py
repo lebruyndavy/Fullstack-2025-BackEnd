712a6bbd56eceb1e995b719742a4d282201acd97
Kenzo <r0930346@student.thomasmore.be>
2025-06-02 22:00:17 +0200

    added routes, queries and models for student 2

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 import database
-from queries import student1_queries as queries
+from queries import student1_queries as queries_student1
+from routes import get_endpoints_student2, post_endpoints_student2
 import config
 
 app = FastAPI(docs_url=config.documentation_url)
+
+app.include_router(router=get_endpoints_student2.app)
+app.include_router(router=post_endpoints_student2.app)
 
 @app.get("/")
 def root():
@@ -11,7 +15,7 @@ def root():
 
 @app.get("/portfolio_items")
 def get_all_portfolio_items():
-    query = queries.portfolio_names_query
+    query = queries_student1.portfolio_names_query
     portfolio_items = database.execute_sql_query(query)
     if isinstance(portfolio_items, Exception):
         return portfolio_items, 500
@@ -19,3 +23,6 @@ def get_all_portfolio_items():
     for portfolio_item in portfolio_items:
         portfolio_items_to_return.append(portfolio_item[0])
     return {'portfolio_items': portfolio_items_to_return}
+
+
+
